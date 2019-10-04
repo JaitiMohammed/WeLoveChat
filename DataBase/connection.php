2071477d69bcecfa41e217e589912abfed9ce66b
Jaiti Mohammed <38511742+JaitiMohammed@users.noreply.github.com>
2019-10-05 00:00:24 +0100

    We Love Chat Commit 1

---
+++ DataBase/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","social");
+if(mysqli_connect_errno()){
+  echo "Failed to Connect".mysqli_connect_errno();
+}
+
+
+
+
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('DataBase/connection.php'); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to We Love Chat</title>
+  </head>
+  <body>
+
+  </body>
+</html>
